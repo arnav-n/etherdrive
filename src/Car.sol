27279ad4965c0f7269319315145e8601ce6f3291
Alex Granados <alexg@MacBook-Air-4.local>
2024-06-13 16:38:05 -0700

    updated car contract

--- src/Car.sol
+++ src/Car.sol
@@ -5,9 +5,22 @@ pragma solidity ^0.8.0;
 import "./User.sol";
 
 contract Car{
-    User curOwner;
+    address curOwner;
     string make;
     string model;
     int year;
     int VIN;
+
+    constructor(address _owner, string memory _make, string memory _model, int _year, int _vin) {
+        curOwner = _owner;
+        make = _make;
+        model = _model;
+        year = _year;
+        VIN = _vin;
+    }
+
+    function transferOwnership(address newOwner) public {
+        require(msg.sender == curOwner, "only the owner can transfer ownership");
+        curOwner = newOwner;
+    }
 }
